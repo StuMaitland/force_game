0d682b0beebd71080cde4b392c62c26a8c072785
Stuart Maitland <stuart@mediwikis.com>
2021-02-09 11:19:23 +0000

    Update start_react_game.py

--- start_react_game.py
+++ start_react_game.py
@@ -1,21 +1,33 @@
 import startreactreach
 import random
 import time
-
-opts = [1, 2, 3, 4]
+import csv
+import datetime
 
 startreactreach.init()
 startreactreach.target(-1)
 
-while 1:
-    btn_state=0
+response_times = []
+trial_types = []
+i = 0
+
+while i < 10:
+    i += 1
+    btn_state = 0
     time.sleep(1)
-    startreactreach.dostartreact(random.randint(0, 2))
+    trial = random.randint(0, 2)
+    startreactreach.dostartreact(trial)
     time_a = time.time()
     while btn_state:
         btn_val = startreactreach.button()
         btn_state = btn_val != b'\x1f'
     startreactreach.target(-1)
     time_b = time.time()
-    response_time = time_b-time_a
-    print(response_time)
+    response_times.append(time_b - time_a)
+    trial_types.append(trial)
+
+with open('start_react_{}.txt'.format(datetime.now()), mode='w') as f:
+    writer = csv.writer(f)
+    for k, v in enumerate(response_times):
+        writer.writerow([trial_types[k]] + response_times[k])
+
