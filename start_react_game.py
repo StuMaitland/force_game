27f0e8ecfa34c839a271a03f326a64f787ccb055
Stuart Maitland <stuart@mediwikis.com>
2021-02-09 10:39:06 +0000

    Update start_react_game.py

--- start_react_game.py
+++ start_react_game.py
@@ -4,15 +4,13 @@ import time
 
 buttons = [1, 2, 3, 4]
 
-#startreactreach.init()
+startreactreach.init()
 
 while 1:
     random.shuffle(buttons)
     for button in buttons:
-        #startreactreach.target(0)
-        print(0)
+        startreactreach.target(0)
         time.sleep(1)
 
-        #startreactreach.target(button)
-        print(button)
+        startreactreach.target(button)
         time.sleep(2)
