15ef50f6aa50b7ccdeec7bc10947003466c5ccc5
Stuart Maitland <stuart@mediwikis.com>
2021-02-09 10:46:13 +0000

    Added startreact protocol

--- start_react_game.py
+++ start_react_game.py
@@ -2,15 +2,10 @@ import startreactreach
 import random
 import time
 
-buttons = [1, 2, 3, 4]
+opts = [1, 2, 3, 4]
 
 startreactreach.init()
 
 while 1:
-    random.shuffle(buttons)
-    for button in buttons:
-        startreactreach.target(0)
-        time.sleep(1)
-
-        startreactreach.target(button)
-        time.sleep(2)
+    time.sleep(1)
+    startreactreach.dostartreact(random.randint(0, 2))
---
+++ target_reach_game.py
@@ -0,0 +1,14 @@
+import startreactreach
+import random
+import time
+
+buttons = [1, 2, 3, 4]
+
+while 1:
+    random.shuffle(buttons)
+    for button in buttons:
+        startreactreach.target(0)
+        time.sleep(1)
+
+        startreactreach.target(button)
+        time.sleep(2)
